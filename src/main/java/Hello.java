aa2afcbbc14398eb21dda21fec3eca3a90e414e6
Shinelon <1052536165@qq.com>
2020-09-09 16:43:09 +0800

    创建一个项目

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+     public void  hello(){
+          System.out.println("hello");
+
+
+     }
+
+     public static void main(String[] args) {
+          System.out.println("");
+     }
+}
